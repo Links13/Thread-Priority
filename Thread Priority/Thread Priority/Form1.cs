c9abd5fc64d9811a4288e7710d1be57a4ff0cce0
Links13 <148409127+Links13@users.noreply.github.com>
2023-10-23 18:01:36 +0800

    Add files via upload

---
+++ Thread Priority/Thread Priority/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thread_Priority
+{
+    public partial class frmTrackThread : Form
+    {
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTrackThread_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart delThread = new ThreadStart(MyThreadClass.Thread1);
+            ThreadStart delThread2 = new ThreadStart(MyThreadClass.Thread2);
+
+            Thread ThreadA = new Thread(delThread);
+            ThreadA.Name = "Thread A Process";
+
+            Thread ThreadB = new Thread(delThread2);
+            ThreadB.Name = "Thread B Process";
+
+            Thread ThreadC = new Thread(delThread);
+            ThreadC.Name = "Thread C Process";
+
+            Thread ThreadD = new Thread(delThread2);
+            ThreadD.Name = "Thread D Process";
+
+
+            Console.WriteLine("Before the thread start");
+
+           
+            ThreadA.Priority = ThreadPriority.Highest;
+            ThreadB.Priority = ThreadPriority.Normal;
+            ThreadC.Priority = ThreadPriority.AboveNormal;
+            ThreadD.Priority = ThreadPriority.BelowNormal;
+
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadC.Start();
+            ThreadD.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+            ThreadC.Join();
+            ThreadD.Join();
+            label1.Text = "-End of Thread-";
+            Console.WriteLine(label1.Text.ToString());
+        }
+    }
+    
+}
---
+++ Thread Priority/Thread Priority/MyThreadClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Priority
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+
+            for (int LoopCount = 0; LoopCount < 3; LoopCount++)
+            {
+
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + LoopCount);
+                Thread.Sleep(500);
+            }
+        }
+        public static void Thread2()
+        {
+
+            for (int x = 0; x <= 5; x++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + x);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
+
